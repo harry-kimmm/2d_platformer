3909e4d3c80892d84a0a88a1956f0384949a8fde
Harry Kim <harryk9395@gmail.com>
2023-07-31 13:44:23 -0700

    Implement basic level, character movement, and collisions

---
+++ level.py
@@ -0,0 +1,78 @@
+import pygame
+from tiles import Tile
+from settings import tile_size, screen_width
+from player import Player
+
+class Level:
+    def __init__(self, level_data, surface):
+        # Level Setup
+        self.display_surface = surface
+        self.setup_level(level_data)
+        self.world_shift = 0
+
+    def setup_level(self, layout):
+        self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
+        for r, row in enumerate(layout):
+            for c, cell in enumerate(row):
+                x = c * tile_size 
+                y = r * tile_size
+                if cell == 'X':
+                    tile = Tile((x, y), tile_size)
+                    self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
+    def horizontal_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+
+    def vertical_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+
+
+    def run(self):
+        # Tiles
+        self.tiles.update(self.world_shift)
+        self.tiles.draw(self.display_surface)
+        self.scroll_x()
+
+        # Player
+        self.player.update()
+        self.horizontal_collision()
+        self.vertical_collision()
+        self.player.draw(self.display_surface)
+        
---
+++ settings.py
@@ -0,0 +1,18 @@
+level_map = [
+    '                             ',
+    '                             ',
+    '   XXX                       ',
+    '         X                 X ',
+    '                             ',
+    '              P              ',
+    '              XX             ',
+    '             XX              ',
+    '           XXXX    XX  XX    ',
+    '           XXXX    XX  XXX   ',
+    '    XXXX   XXXXXX  XX  XXXX  ',
+    'XXXXXXXX   XXXXXX  XX  XXXX  '
+]
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
